b7d082b92b118865cc7b12f41249716b7d1293af
Scott Ankiewicz <scottfa.a@gmail.com>
2023-05-30 19:57:43 -0400

    wip: set up database schema

--- public/database.js
+++ public/database.js
@@ -4,4 +4,22 @@ const database = new sqlite3.Database('./public/db.sqlite3', (e) => {
   if (e) console.error('Database opening error: ', e)
 })
 
+database
+  .exec(
+    `CREATE TABLE IF NOT EXISTS Images(
+      uid TEXT,
+      rating INTEGER,
+      tags REAL,
+      flag INTEGER DEFAULT 0 CHECK (flag BETWEEN -1 and 1),
+      collections REAL,
+      note TEXT
+    )`
+  )
+  .exec(
+    `CREATE TABLE IF NOT EXISTS Collections(
+      name TEXT,
+      description TEXT
+    )`
+  )
+
 module.exports = { database }
