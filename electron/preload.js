3b22cb52a0c70eae848b6d393964de8e8c5c4f5b
Scott Ankiewicz <scottfa.a@gmail.com>
2023-06-20 17:12:58 -0400

    wip: create API path to update images

--- electron/preload.js
+++ electron/preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('message', {
   indexImages: () => ipcRenderer.send('indexImages'),
   importImages: () => ipcRenderer.send('importImages'),
   forgetImage: (id) => ipcRenderer.send('forgetImage', id),
+  updateImage: (params) => ipcRenderer.send('updateImage', params),
 })
 
 ipcRenderer.on('indexImages', (event, response) =>
--- electron/utilities/databaseMessage.js
+++ electron/utilities/databaseMessage.js
@@ -28,11 +28,40 @@ class DatabaseHandler {
       if (!this.changes) event.reply('error', 'delete failed to match rowid')
     })
   }
+
+  updateImage(event, params) {
+    if (!params.rowid) {
+      event.reply('error', 'updates require a rowid property')
+      return
+    }
+
+    const validKeys = ['uid', 'rating', 'tags', 'flag', 'collections', 'note']
+    const paramKeys = Object.keys(params)
+    let sqlParams = []
+
+    let sql = `UPDATE Images`
+    for (let key of validKeys) {
+      if (paramKeys.find((element) => element === key)) {
+        sql += `\nSET ${key} = ?`
+        sqlParams.push(params[key])
+      }
+    }
+    sql += `\nWHERE rowid = ?`
+    sqlParams.push(params.rowid)
+
+    if (sqlParams.length === 1) {
+      event.reply('error', 'update did not contain valid column keys')
+      return
+    }
+
+    this.database.run(sql, sqlParams)
+  }
 }
 
 const databaseHandler = new DatabaseHandler()
 
 ipcMain.on('indexImages', databaseHandler.indexImages.bind(databaseHandler))
 ipcMain.on('forgetImage', databaseHandler.deleteImage.bind(databaseHandler))
+ipcMain.on('updateImage', databaseHandler.updateImage.bind(databaseHandler))
 
 module.exports = { databaseHandler }
